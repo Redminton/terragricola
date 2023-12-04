50ff7ff205c68804cbd796e3556f411ab9d7eb7c
pc gui <133177595+Redminton@users.noreply.github.com>
2023-12-04 06:57:46 -0300

    v3.5

--- js/conta.js
+++ js/conta.js
@@ -1,7 +1,54 @@
- function expandLogin() {
-    document.getElementById("login").style.display = "block";
+ 
+ const login_admin = 'admin';
+ const senha_admin = 'admin';
+ const login_conta1 = 'conta1';
+ const senha_conta1 = 'conta1';
+ 
+ 
+ 
+ 
+ function login() {
+    //pega o id do div e transforma em uma variável
+    var login = document.getElementById("login");
+    var estado_atual = window.getComputedStyle(login).display;
+
+    // Verificar o estado atual do div
+    if (estado_atual === "none") {
+        login.style.display = "block";
+    } else {
+        login.style.display = "none";
+    }
+}
+function acessar(){
+    var x = document.getElementById('usuario').value;
+    var y = document.getElementById('senha').value;
+        if (x === login_admin && y === senha_admin) {
+            console.log('Acessando admin');
+            window.location.href = 'admin.html';
+        } 
+        else if (x === login_conta1 && y === senha_conta1) {
+            console.log('Acessando conta');
+            window.location.href = 'minha_conta.html';
+        } 
+        else {
+            alert('Credenciais incorretas. Tente novamente.');
+         
+        }
+
 }
 
-function collapseLogin() {
-    document.getElementById("login").style.display = "none";
+function acessar2(){
+ //pega o id do div e transforma em uma variável
+ var login = document.getElementById("login");
+ var estado_atual = window.getComputedStyle(login).display;
+
+ // Verificar o estado atual do div
+ if (estado_atual === "none") {
+     login.style.display = "block";
+ } else {
+     login.style.display = "none";
+ }
+ acessar();
 }
+
+
